fed5bb4697c1229927a1a5014062be5fc112c698
Taakaestheroundo <144691445+Taakaestheroundo@users.noreply.github.com>
2023-10-19 00:32:10 -0700

    test one

---
+++ qun 2.py
@@ -0,0 +1,15 @@
+ # qun 2
+# Functions
+# given the numbers (x,y)
+numbers = (20,60)
+x = 20
+y = 60
+def age(x,y):
+    average_of_the_numbers =(20+60)/2
+    print(f"he average is{average_of_the_numbers}")
+
+
+# program to input 3 numbers and find the minimum
+math_computing = input("enter")
+python_programming = input("enter")
+data_science = input("enter")
---
+++ qun 3.py
@@ -0,0 +1,19 @@
+
+# QUESTION 3
+# A list
+student_marks = [78,83,90,88,75]
+sum_of_students_marks = [78+83+90+88+75]
+print(sum_of_students_marks)
+
+# display last two numbers(78,75)
+average_of_first_and_last_number = (78,75)
+print("average of first and last number")
+
+# add
+sum_of_students_marks.append(96)
+print(student_marks)
+
+# updating(78,87)
+student_marks = (w) = 87
+student_marks = (T) = 78
+print("updating student marks")
