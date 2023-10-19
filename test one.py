7f6f8e3e0d6ae250efb8743d009dc34551078b97
Taakaestheroundo <144691445+Taakaestheroundo@users.noreply.github.com>
2023-10-19 00:28:41 -0700

    test one .py

---
+++ test one.py
@@ -0,0 +1,32 @@
+ # Given the following variables.in both snake case and camel case
+
+
+# students_ marks = snake case 
+# total Average camel case
+
+# students Marks = camel case
+# total-average = snake case
+
+# convert variable age
+age = 22
+print(type(age))
+
+# new data type
+new_age = float(22)
+print(new_age)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
